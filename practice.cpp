b14e722c35a79ba4327663cae78431c4e5eeae5c
Himanshu Joshi <hiimanshuhh16@gmail.com>
2025-04-15 23:29:41 +0530

    practicing

--- practice.cpp
+++ practice.cpp
@@ -1,24 +1,10 @@
 #include<iostream>
-#include<vector>
-#include<unordered_set>
+#include<string>
 using namespace std;
-bool checkDup(vector<int>arr){
-  unordered_set<int>set;
-  for(int i=0; i<arr.size(); i++){
-    if(set.count(arr[i])){
-      return true;
-    }
-    set.insert(arr[i]);
-  }
-  return false;
-}
 int main(){
-  vector<int>arr{1,2,3,4,5,1,2};
-  checkDup(arr);
-  if(checkDup(arr)){
-    cout<<"Duplicate Found!";
-  }else{
-    cout<<"No Duplicate Found!";
-  }
-  return 0;
+  string s1 = "Himanshu Joshi";
+  string s2("is a very");
+  string s3;
+  s3 = "genuine guy.";
+  cout<<s1<<" "<<s2<<" "<<s3;
 }
