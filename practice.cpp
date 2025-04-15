a2bd2c2017ad13e04d856ec3a2907aca98252cb9
Himanshu Joshi <hiimanshuhh16@gmail.com>
2025-04-15 23:59:15 +0530

    practicing

--- practice.cpp
+++ practice.cpp
@@ -1,10 +1,17 @@
 #include<iostream>
+#include<vector>
 #include<string>
 using namespace std;
-int main(){
-  string s1 = "Himanshu Joshi";
-  string s2("is a very");
-  string s3;
-  s3 = "genuine guy.";
-  cout<<s1<<" "<<s2<<" "<<s3;
+vector<string> fizzBuzz(int n){
+  vector<string>ans;
+  for(int i=1; i<=n; i++){
+    string temp = "";
+    
+    if(i % 3 == 0)temp += "Fizz";
+    if(i % 5 == 0)temp += "Buzz";
+    if(temp == "")temp = to_string(i);
+
+    ans.push_back(temp);
+  }
+  return ans;
 }
